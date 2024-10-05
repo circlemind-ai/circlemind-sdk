6ac6a0f96e3a0840c6fca36c4d11089a63018355
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2024-10-04 19:02:11 -0700

    Add verbose argument

--- tests/test_all.py
+++
@@ -1,8 +0,0 @@
-from circlemind import Circlemind
-
-if __name__ == '__main__':
-    cm = Circlemind("YOUR_API_KEY", "http://ec2-3-81-205-157.compute-1.amazonaws.com")
-
-    memories = cm.get_memories("What animals do I like?")
-    
-    print(memories)
